1167d955f6e1fae18c3153a6357722bb828beb0b
FrostElAkuma <Vilgaks2002@yahoo.com>
2023-01-06 14:48:54 +0400

    Payment Page, Started order controller

---
+++ lib/controllers/order_controller.dart
@@ -0,0 +1,4 @@
+import 'package:get/get.dart';
+
+//We use GetxService So we can use the OrderController thro out our app and not be restricted to a certain page
+class OrderController extends GetxController implements GetxService {}
